4cc0cbef7fc8cbf6638d3e80ee93027006b21855
Jeison Tomio <medriase@gmail.com>
2023-04-28 10:47:40 -0300

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import { Heading } from '@ignite-ui/react'
+
+export default function Home() {
+  return (
+    <div className="w-10">
+      <Heading as="h1">Hello World</Heading>
+    </div>
+  )
+}
