89ea26864c2ea6fe415083ef4332608a04895429
Jeison Tomio <medriase@gmail.com>
2023-05-04 16:53:04 -0300

    Zod validation ClaimUsernameForm

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,32 @@
-import { Heading } from '@ignite-ui/react'
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+
+import Image from 'next/image'
+
+import previewImage from '../../assets/app-preview.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
 
 export default function Home() {
   return (
-    <div className="w-10">
-      <Heading as="h1">Hello World</Heading>
-    </div>
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+        <ClaimUsernameForm />
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          priority
+          alt="Calendário simbolizando aplicação"
+        />
+      </Preview>
+    </Container>
   )
 }
